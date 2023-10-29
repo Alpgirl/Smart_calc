778c7e2cc6247620da01bdd793780d4bcea15e6a
Inna <cool.inna-24@yandex.ru>
2023-10-30 00:55:05 +0300

    Deleted comments

--- src/test.sh
+++ src/test.sh
@@ -1,16 +1,3 @@
 #!/bin/bash
 
-# # #Directory for the html-report(deleting)
-# rm -rf report
-
-# #Testing
-# ctest --test-dir build
-
 make test
-
-# # #Directory for the report (creating)
-# mkdir report
-
-# # #HTML - report
-# lcov -c -d . -o ./report/name.info
-# genhtml  ./report/name.info -o report
