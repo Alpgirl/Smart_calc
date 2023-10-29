54ff43112e653d54d170ace2c12aaa0412e79e2b
georgy <georgy.shulyndin@gmail.com>
2023-10-29 22:50:00 +0300

    deleted test goal from build.sh

--- src/build.sh
+++ src/build.sh
@@ -3,9 +3,7 @@
 function display_usage {
     echo "Usage: $0 <target>"
     echo "Targets:"
-    echo "  all: Build the library (calc.a) and run tests"
     echo "  calc.a: Build the library (calc.a)"
-    echo "  test: Run unit tests"
     echo "  codestyle: Apply Google code style to the project"
     echo "  clean: Clean build files"
     echo "  run: Build and run the command line calculator"
@@ -19,17 +17,9 @@ fi
 
 # Check the target and execute the corresponding command
 case "$1" in
-    "all")
-        # Build the library (calc.a) and run tests
-        make all
-        ;;
     "calc.a")
         # Build the library (calc.a)
         make calc.a
-        ;;
-    "test")
-        # Run unit tests
-        make test
         ;;
     "codestyle")
         # Apply Google code style to the project
